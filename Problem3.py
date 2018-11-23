6c1daa409f894d4c2c4d07b0eb58805e241d4405
dghohens <dghohens@gmail.com>
2018-11-22 19:23:29 -0500

    Day 22 of 100 days of code, working on project euler problems.

--- Problem3.py
+++ Problem3.py
@@ -10,21 +10,33 @@ square_root = int(sqrt(600851475143))
 
 possible_list = [i for i in range(3, square_root + 1, 2)]
 
+class create_sieve:
+    def __init__(self, inlist, sievenum, endnum):
+        self.inlist = inlist
+        self.sievenum = sievenum
+        self.endnum = endnum
+
+    def extend_sieve(self):
+        for i in range(self.sievenum, self.endnum, 2 * self.sievenum):
+            if i not in self.inlist:
+                self.inlist.append(i)
+        return self.inlist
+
+
 def find_primes(in_list):
-    prime_list = []
-    is_divisible = True
-    x = lambda a, b: a % b == 0
-    for i in in_list:
-        for j in in_list[:in_list.index(i)]:
-            is_divisible = x(i,j)
-            if is_divisible == True:
-                print(i)
-                print(j)
-                print()
-                break
-        if is_divisible == False:
+    prime_list = [2, 3]
+    x = create_sieve(prime_list, 3, square_root)
+    composite_list = x.extend_sieve()
+    for i in possible_list:
+        if i in composite_list:
+            pass
+        else:
             prime_list.append(i)
-    return prime_list
+            print(i)
+            x = create_sieve(composite_list, i, square_root)
+            composite_list.append(x.extend_sieve)
+            print(composite_list)
+
 
 
 primes = find_primes(possible_list)
